ca48af737335d430d7276aad024cc48a2a11c97f
dbarriguete@gmail.com <dbarriguete@gmail.com>
2021-08-31 08:19:56 -0500

    Examples August 31st, 2021

---
+++ Exe01/01OperatingNVariables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01OperatingNVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int sumResult = 2 + 3;
+            int operationResult = 2 * 3;
+            operationResult = 10/5; // example with binary operators
+            operationResult = 3-2;
+
+            operationResult = 26 % 4;
+
+            operationResult = sumResult++; //example with unary operators
+
+            // var sumResult = $"2 + 3 = {2+3}";
+            // int sumResult;
+
+            // Console.WriteLine("The result of the operation is: ");
+            // Console.WriteLine($"2 + 3 = {sumResult}");
+            // Console.WriteLine($"The value of other operation: {operationResult}");
+            Console.WriteLine("Variables' values:");
+            Console.WriteLine("");
+        }
+    }
+}
