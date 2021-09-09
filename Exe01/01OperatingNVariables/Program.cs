aeca56d713b2546abc142ea7c2d40ad49199404d
dbarriguete@gmail.com <dbarriguete@gmail.com>
2021-09-09 10:43:38 -0500

    Explanationa about unary operators, logical operators and condition logical operators.
    follow the streaming at Twitch: https://twitch.tv/D1366er

--- Exe01/01OperatingNVariables/Program.cs
+++ Exe01/01OperatingNVariables/Program.cs
@@ -23,9 +23,87 @@ namespace _01OperatingNVariables
             // Console.WriteLine("The result of the operation is: ");
             // Console.WriteLine($"2 + 3 = {sumResult}");
             // Console.WriteLine($"The value of other operation: {operationResult}");
-            Console.WriteLine("Variables' values:");
-            Console.WriteLine($"Sum Result: {sumResult}");
-            Console.WriteLine($"Operation Result: {operationResult}");
+
+
+            // Console.WriteLine("Variables' values:");
+            // Console.WriteLine($"Sum Result: {sumResult}");
+            // Console.WriteLine($"Operation Result: {operationResult}");
+
+            ////Using unary operators
+            //int autoSum = 1;
+            //Console.WriteLine($"Using unary operators: {autoSum}");
+            //autoSum++;
+            //Console.WriteLine($"after using ++ {autoSum}");
+            //autoSum--;
+            //Console.WriteLine($"after using -- {autoSum}");
+            //Console.WriteLine("");
+            //Console.WriteLine("########################################################");
+            //Console.WriteLine("");
+            //autoSum=20;
+            //Console.WriteLine($"After set to {autoSum} using ++ in the end {autoSum++}");
+            //Console.WriteLine($"After the writeline {autoSum}");
+
+
+            //Console.WriteLine($"Current value {autoSum} using ++ in the beginning {++autoSum}");
+            //Console.WriteLine($"After the writeline {autoSum}");
+
+            //autoSum=20;
+            //Console.WriteLine($"After set to {autoSum} using -- in the end {autoSum--}");
+            //Console.WriteLine($"After the writeline {autoSum}");
+
+
+            //Console.WriteLine($"Current value {autoSum} using -- in the beginning {--autoSum}");
+            //Console.WriteLine($"After the writeline {autoSum}");
+
+            //// Assignment operators
+            //int operand01 = 20;
+            //Console.WriteLine($"Operand value {operand01}");
+            //operand01 += 5;
+            //Console.WriteLine($"after += 5 : {operand01}");
+            //operand01 -= 3;
+            //Console.WriteLine($"after -= 3 : {operand01}");
+            //operand01 *= 2;
+            //Console.WriteLine($"after *= 2 : {operand01}");
+            //operand01 /= 2;     
+            //Console.WriteLine($"after /= 2 : {operand01}");
+
+            bool boolVariableA = true;
+            bool boolVariableB = false;
+
+            // Logical operators
+            //Console.WriteLine($"AND |      A |      B");
+            //Console.WriteLine($"A   | {boolVariableA & boolVariableA}   | {boolVariableA&         boolVariableB}");
+            //Console.WriteLine($"B   | {boolVariableB & boolVariableA}  | {boolVariableB&         boolVariableB}");
+            //Console.WriteLine("");
+            //Console.WriteLine($"OR  |      A |      B");
+            //Console.WriteLine($"A   | {boolVariableA | boolVariableA}   | {boolVariableA|boolVariableB}");
+            //Console.WriteLine($"B   | {boolVariableB | boolVariableA}   | {boolVariableB|boolVariableB}");
+            //Console.WriteLine("");
+            //Console.WriteLine($"XOR |      A |      B");
+            //Console.WriteLine($"A   | {boolVariableA ^ boolVariableA}  | {boolVariableA^boolVariableB}");
+            //Console.WriteLine($"B   | {boolVariableB ^ boolVariableA}   | {boolVariableB^boolVariableB}");
+
+            // Logical operators AND Conditional logical operators.
+            Console.WriteLine($" A & AdditionalFunction() = {boolVariableA & AdditionalFunction()} ");
+            Console.WriteLine($" B & AdditionalFunction() = {boolVariableB & AdditionalFunction()} ");
+
+            Console.WriteLine($" A && AdditionalFunction() = {boolVariableA && AdditionalFunction()} ");
+            Console.WriteLine($" B && AdditionalFunction() = {boolVariableB && AdditionalFunction()} ");
+
+            Console.WriteLine($" A | AdditionalFunction() = {boolVariableA | AdditionalFunction()} ");
+            Console.WriteLine($" B | AdditionalFunction() = {boolVariableB | AdditionalFunction()} ");
+
+            Console.WriteLine($" A || AdditionalFunction() = {boolVariableA || AdditionalFunction()} ");
+            Console.WriteLine($" B || AdditionalFunction() = {boolVariableB || AdditionalFunction()} ");
+            
+        }
+
+
+        private static bool AdditionalFunction()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("I'm doing something here, just give me a second.");
+            return true;
         }
     }
 }
