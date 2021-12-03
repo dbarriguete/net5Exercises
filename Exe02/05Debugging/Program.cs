4b9c13410cc9b159be18d33755bf3ac829f14f15
dbarriguete@gmail.com <dbarriguete@gmail.com>
2021-12-03 12:40:00 -0600

    2021_12_03 Using the debugger in VSCode

---
+++ Exe02/05Debugging/Program.cs
@@ -0,0 +1,11 @@
+﻿//in dotnet 6 you have implicit use of these directives in Program.cs file
+// using System;
+// using System.IO;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Net.Http;
+// using System.Threading;
+// using System.Threading.Tasks;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
---
+++ Exe02/05DebuggingDotNet5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _05DebuggingDotNet5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I calculate the Yearly amount if you have the monthly amount.");
+            Console.Write ("What is the monthly amount? ");
+            // string input = Console.ReadLine();
+
+            string input = "2.1";
+            decimal monthlyAmount=0;
+            if (decimal.TryParse(input, out monthlyAmount ))
+            {
+                if(monthlyAmount<=0)
+                {
+                    Console.WriteLine("The number is invalid, only use a number greater than 0.");
+                }
+                else
+                {
+                    Console.WriteLine($"The yearly amount of {monthlyAmount:C} is: {GetYearlyAmount(monthlyAmount):C}");
+                }
+            }
+            else 
+            {
+                Console.WriteLine("The information provided is not a decimal, only provide decimal values greater than 0.");
+            }
+        }
+
+
+        static decimal GetYearlyAmount (decimal monthlyAmount)
+        {
+            return (monthlyAmount*12);
+        }
+    }
+}
